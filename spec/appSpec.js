e9bc4209c242cabfba5470d91cd638c25391a058
Ayoub <ayoub.benhammou@student.vinci.be>
2024-11-13 18:23:48 +0100

    add: manage the case 5 for function minutesOnePrint

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -104,143 +104,144 @@ describe("HoursFive function should return the answer", function () {
   });
 });
 
-
-
 describe("Minutes function should return the answer", function () {
-    const app = new BerlinClock();
-  
-    it("Minutes should return 0 when given 0", function () {
-      const result = app.minutesOne(0);
-  
-      expect(result).toBe(0);
-    });
-  
-    it("Minutes should return 1 when given 1", function () {
-      const result = app.minutesOne(1);
-  
-      expect(result).toBe(1);
-    });
-  
-    it("Minutes should return 2 when given 2", function () {
-      const result = app.minutesOne(2);
-  
-      expect(result).toBe(2);
-    });
-  
-    it("Minutes should return 3 when given 3", function () {
-      const result = app.minutesOne(3);
-  
-      expect(result).toBe(3);
-    });
-  
-    it("Minutes should return 4 when given 4", function () {
-      const result = app.minutesOne(4);
-  
-      expect(result).toBe(4);
-    });
-  
-    it("Minutes should return 0 when given 5", function () {
-      const result = app.minutesOne(5);
-  
-      expect(result).toBe(0);
-    });
-  
-    it("Minutes should return 1 when given 36", function () {
-      const result = app.minutesOne(36);
-  
-      expect(result).toBe(1);
-    });
-  });
-  
-  describe("MinutesFive function should return the answer", function () {
-    const app = new BerlinClock();
-  
-    it("MinutesFive should return 0 when given 0", function () {
-      const result = app.minutesFive(0);
-  
-      expect(result).toBe(0);
-    });
-  
-    it("MinutesFive should return 1 when given 5", function () {
-      const result = app.minutesFive(5);
-  
-      expect(result).toBe(1);
-    });
-    
-    it("MinutesFive should return 1 when given 6", function () {
-      const result = app.minutesFive(6);
-  
-      expect(result).toBe(1);
-    });
-  
-    it("MinutesFive should return 2 when given 10", function () {
-      const result = app.minutesFive(10);
-  
-      expect(result).toBe(2);
-    });
-  
-    it("MinutesFive should return 11 when given 55", function () {
-      const result = app.minutesFive(55);
-  
-      expect(result).toBe(11);
-    });
-  });
-  
-  
-  describe("Seconds function should return the answer", function () {
-    const app = new BerlinClock();
-  
-    it("Seconds should return X when given 1", function () {
-      const result = app.seconds(1);
-  
-      expect(result).toBe("X");
-    });
-  
-    it("Seconds should return R when given 2", function () {
-      const result = app.seconds(2);
-  
-      expect(result).toBe("R");
-    });
-  
-    it("Seconds should return X when given an odd number", function () {
-      const result = app.seconds(3);
-  
-      expect(result).toBe("X");
-    });
-  
-    it("Seconds should return R when given an even number", function () {
-      const result = app.seconds(4);
-  
-      expect(result).toBe("R");
-    });
+  const app = new BerlinClock();
+
+  it("Minutes should return 0 when given 0", function () {
+    const result = app.minutesOne(0);
+
+    expect(result).toBe(0);
   });
 
-  describe("MinutesOnePrint function should return the answer", function () {
-    const app = new BerlinClock();
-  
-    it("MinutesOnePrint should return YXXX when given 1", function () {
-      const result = app.minutesOnePrint(1);
+  it("Minutes should return 1 when given 1", function () {
+    const result = app.minutesOne(1);
 
-      expect(result).toBe("YXXX");
-    });
-
-    it("MinutesOnePrint should return YYXX when given 2", function () {
-      const result = app.minutesOnePrint(2);
-
-      expect(result).toBe("YYXX");
-    });
-    
-    it("MinutesOnePrint should return YYYX when given 3", function () {
-        const result = app.minutesOnePrint(3);
-  
-        expect(result).toBe("YYYX");
-      });
-
-      it("MinutesOnePrint should return YYYY when given 4", function () {
-        const result = app.minutesOnePrint(4);
-  
-        expect(result).toBe("YYYY");
-      });
-
-
+    expect(result).toBe(1);
   });
+
+  it("Minutes should return 2 when given 2", function () {
+    const result = app.minutesOne(2);
+
+    expect(result).toBe(2);
+  });
+
+  it("Minutes should return 3 when given 3", function () {
+    const result = app.minutesOne(3);
+
+    expect(result).toBe(3);
+  });
+
+  it("Minutes should return 4 when given 4", function () {
+    const result = app.minutesOne(4);
+
+    expect(result).toBe(4);
+  });
+
+  it("Minutes should return 0 when given 5", function () {
+    const result = app.minutesOne(5);
+
+    expect(result).toBe(0);
+  });
+
+  it("Minutes should return 1 when given 36", function () {
+    const result = app.minutesOne(36);
+
+    expect(result).toBe(1);
+  });
+});
+
+describe("MinutesFive function should return the answer", function () {
+  const app = new BerlinClock();
+
+  it("MinutesFive should return 0 when given 0", function () {
+    const result = app.minutesFive(0);
+
+    expect(result).toBe(0);
+  });
+
+  it("MinutesFive should return 1 when given 5", function () {
+    const result = app.minutesFive(5);
+
+    expect(result).toBe(1);
+  });
+
+  it("MinutesFive should return 1 when given 6", function () {
+    const result = app.minutesFive(6);
+
+    expect(result).toBe(1);
+  });
+
+  it("MinutesFive should return 2 when given 10", function () {
+    const result = app.minutesFive(10);
+
+    expect(result).toBe(2);
+  });
+
+  it("MinutesFive should return 11 when given 55", function () {
+    const result = app.minutesFive(55);
+
+    expect(result).toBe(11);
+  });
+});
+
+describe("Seconds function should return the answer", function () {
+  const app = new BerlinClock();
+
+  it("Seconds should return X when given 1", function () {
+    const result = app.seconds(1);
+
+    expect(result).toBe("X");
+  });
+
+  it("Seconds should return R when given 2", function () {
+    const result = app.seconds(2);
+
+    expect(result).toBe("R");
+  });
+
+  it("Seconds should return X when given an odd number", function () {
+    const result = app.seconds(3);
+
+    expect(result).toBe("X");
+  });
+
+  it("Seconds should return R when given an even number", function () {
+    const result = app.seconds(4);
+
+    expect(result).toBe("R");
+  });
+});
+
+describe("MinutesOnePrint function should return the answer", function () {
+  const app = new BerlinClock();
+
+  it("MinutesOnePrint should return YXXX when given 1", function () {
+    const result = app.minutesOnePrint(1);
+
+    expect(result).toBe("YXXX");
+  });
+
+  it("MinutesOnePrint should return YYXX when given 2", function () {
+    const result = app.minutesOnePrint(2);
+
+    expect(result).toBe("YYXX");
+  });
+
+  it("MinutesOnePrint should return YYYX when given 3", function () {
+    const result = app.minutesOnePrint(3);
+
+    expect(result).toBe("YYYX");
+  });
+
+  it("MinutesOnePrint should return YYYY when given 4", function () {
+    const result = app.minutesOnePrint(4);
+
+    expect(result).toBe("YYYY");
+  });
+
+  it("MinutesOnePrint should return XXXX when given 5", function () {
+    const result = app.minutesOnePrint(5);
+
+    expect(result).toBe("XXXX");
+  });
+});
